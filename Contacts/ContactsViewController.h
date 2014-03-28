84843ca588b483b3e965579f0368c5daf17addf2
toddclaussen <s0468413@student.rockvalleycollege.edu>
2014-03-28 15:43:19 -0500

    Contacts

--- Contacts/ContactsViewController.h
+++ Contacts/ContactsViewController.h
@@ -13,13 +13,22 @@
 @property (strong, nonatomic) IBOutlet UIButton *btnBack;
 @property (strong, nonatomic) IBOutlet UITextView *txtData;
 @property (strong, nonatomic) IBOutlet UIScrollView *scrollview;
+
 @property (strong, nonatomic) IBOutlet UITextField *txtName;
+
 @property (strong, nonatomic) IBOutlet UITextField *txtAddress;
+
 @property (strong, nonatomic) IBOutlet UITextField *txtCity;
+
 @property (strong, nonatomic) IBOutlet UITextField *txtState;
+
 @property (strong, nonatomic) IBOutlet UITextField *txtZip;
+
+
 @property (strong, nonatomic) IBOutlet UITextField *txtPhone;
 @property (strong, nonatomic) IBOutlet UITextField *txtEmail;
+
+
 - (IBAction)btnViewData:(id)sender;
 - (IBAction)btnSave:(id)sender;
 -(IBAction) doneEditing:(id) sender;
--- Contacts/ContactsViewController.m
+++ Contacts/ContactsViewController.m
@@ -23,7 +23,9 @@
                                    action:@selector(dismissKeyboard)];
     
     [self.view addGestureRecognizer:tap];
-    
+    CGSize size = [self getScreenSize];
+    self.scrollview .frame = CGRectMake(0, 50, size.width, size.height);
+
 }
 -(void)dismissKeyboard {
     // add textfields and textviews
@@ -49,13 +51,67 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)btnBack:(id)sender {
-}
 - (IBAction)btnViewData:(id)sender {
+    //hide keyboard
+    [self dismissKeyboard];
+    //load voew
+    CGPoint scrollPoint = CGPointMake(0, self.btnBack.frame.origin.y);
+    [self.scrollview  setContentOffset:scrollPoint animated:YES];
+
+}
+
+- (CGSize)getScreenSize
+{
+    //Get Screen size
+    CGSize size;
+    if (UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation) && [[UIScreen mainScreen] bounds].size.height > [[UIScreen mainScreen] bounds].size.width) {
+        // in Landscape mode, width always higher than height
+        size.width = [[UIScreen mainScreen] bounds].size.height;
+        size.height = [[UIScreen mainScreen] bounds].size.width;
+    } else if (UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].statusBarOrientation) && [[UIScreen mainScreen] bounds].size.height < [[UIScreen mainScreen] bounds].size.width) {
+        // in Portrait mode, height always higher than width
+        size.width = [[UIScreen mainScreen] bounds].size.height;
+        size.height = [[UIScreen mainScreen] bounds].size.width;
+    } else {
+        // otherwise it is normal
+        size.height = [[UIScreen mainScreen] bounds].size.height;
+        size.width = [[UIScreen mainScreen] bounds].size.width;
+    }
+    return size;
 }
 
 - (IBAction)btnSave:(id)sender {
+
+
+//hide keyboard
+[self dismissKeyboard];
+
+//save
+//format
+if ([self.txtData.text  isEqual:@""])
+{
+    self.txtData.text = @"Contacts";
 }
+self.txtData.text = [NSString stringWithFormat:@"%@\n\nName:%@\nAddress:%@\nCity:%@\nState:%@:\nZip:%@:\nPhone:%@:\nEmail:%@",
+                     self.txtData.text,self.txtName.text,self.txtAddress.text,self.txtCity.text,self.txtState.text,self.txtZip.text,self.txtPhone.text,self.txtEmail.text];
+
+self.txtName.text = @"";
+self.txtAddress.text = @"";
+self.txtCity.text = @"";
+self.txtState.text = @"";
+self.txtZip.text = @"";
+self.txtPhone.text = @"";
+self.txtEmail.text = @"";
+
+    CGPoint scrollPoint = CGPointMake(0, self.btnBack.frame.origin.y);
+    [self.scrollview  setContentOffset:scrollPoint animated:YES];
+}
+- (IBAction)btnBack:(id)sender {
+        //get back
+        [self.scrollview setContentOffset:CGPointZero animated:YES];
+}
+
+
 - (void)textFieldDidBeginEditing:(UITextField *)textField {
     CGPoint scrollPoint = CGPointMake(0, textField.frame.origin.y);
     [self.scrollview setContentOffset:scrollPoint animated:YES];
